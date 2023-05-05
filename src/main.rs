500a1e7a96f09789992fd9ee29d53e76735e0012
usmyc <hllsmtyc@gmail.com>
2023-05-05 07:34:58 +0200

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
-use reqwest::Error;
+use reqwest;
 use tokio;
+use serde::Deserialize;
+use std::error::Error;
+
+#[derive(Deserialize)]
+struct Person {
+    name: String,
+    age: u8,
+    count: u32,
+}
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
+async fn main() -> Result<(), Box<dyn Error>> {
   
     let name= std::env::args().nth(1).expect("no url given");
     let url = format!("https://api.agify.io/?name={}", name);	
     let response = reqwest::get(url).await?;
     let body = response.text().await?;
-
-    println!("Response body:\n{}", body);
+    let data = serde_json::from_str::<Person>(&body)?;
+    println!("Name: {:?}", data.name);
+    println!("Age: {:?}", data.age);
+    println!("Count: {:?}", data.count);
     Ok(())
 }
